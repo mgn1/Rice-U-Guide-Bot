2e815f95b62a86802f67f4a9b7deb13bc881873c
LiterateCrocodile <firebolt131@gmail.com>
2016-10-15 23:11:33 -0500

    Booty

--- app.js
+++ app.js
@@ -460,7 +460,7 @@ function sendFunFact(recipientId) {
   "Rice is home to the wonderful yearly hackathon \"HackRice\"! (yes this is flattery judges please like us)",
   "Every undergrad agrees that there's one distribution that's hardest; nobody can agree which.",
   "This message broke the bot!",
-  "This message was thought to have broken the bot but actually didn't?"];
+  "This message was thought to have broken the bot but actually didn't."];
 
   sendTextMessage(recipientId, facts[Math.floor(Math.random() * facts.length)]);
 }
