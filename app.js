e948b1bf843959472e737220fb41214bf0ca1cdf
Madeleine Nikirk <mgn1@rice.edu>
2016-10-16 02:46:32 -0500

    explore

--- app.js
+++ app.js
@@ -617,10 +617,11 @@ function sendDirections(recipientId, messageData) {
         console.log("clarification is " + getUser(recipientId).clarify);
         sendConflictMenu(recipientId, conflict[lastLoc[0].substr(9, lastLoc[0].length)]);
     } else {
-        if (lastLoc[0].length <= 2) {
-            sendTextMessage(recipientId, "I don't recognize that location.");
-        }
-        sendTextMessage(recipientId, lastLoc[0] +" is located at " + lastLoc[1]);
+         if (lastLoc[0].length <= 2) {
+             sendTextMessage(recipientId, "I don't recognize that location. Try again.");
+         } else {
+            sendTextMessage(recipientId, lastLoc[0] + " is located at " + lastLoc[1]);
+         }
     }
 }
 
