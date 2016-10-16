2b86c7993c4b02dde01e408512a4719586c932e0
Madeleine Nikirk <mgn1@rice.edu>
2016-10-15 23:23:09 -0500

    updated responses immediately following quick reply

--- app.js
+++ app.js
@@ -460,8 +460,7 @@ function sendFunFact(recipientId) {
   "The record for \"Most Mazelike Builing\" is a tie between Fondren and Duncan Hall.",
   "Rice is home to the wonderful yearly hackathon \"HackRice\"! (yes this is flattery judges please like us)",
   "Every undergrad agrees that there's one distribution that's hardest; nobody can agree which.",
-  "This message broke the bot!",
-  "This message was thought to have broken the bot but actually didn't."];
+  "Tyler is fun fact whore."];
 
   sendTextMessage(recipientId, facts[Math.floor(Math.random() * facts.length)]);
 }
